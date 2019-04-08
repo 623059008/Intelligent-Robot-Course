577007be6ca49490629095b920d8b97c37fea813
HardLearning <873408849@qq.com>
2019-04-08 16:36:52 +0800

    hw5 && lab4

--- HW5/p3_case1.m
+++ HW5/p3_case1.m
@@ -4,8 +4,6 @@ clc;
 clear all;
 
 
-r = 1;
-t = 2;
 theta = 0;
 delta = 1;
 [a1, a2, a3, a4, a5, a6] = deal(0, 0.03, 0.2, 0.04, 0.03, 0.02);
@@ -13,8 +11,8 @@ delta = 1;
 
 sample = @(x) x * randn(1);
 
-[v0, w0, r0] = deal(1, 1, 0);
-[v, w, r] = deal(v0, w0, r0);
+[v0, w0] = deal(1, 1);
+[v, w] = deal(v0, w0);
 
 tmp1 = a1 * (v0^2) + a2 * (w0^2);
 tmp2 = a3 * (v0^2) + a4 * (w0^2);
@@ -24,7 +22,7 @@ x = [];
 y = [];
 
 for n = 1:500
-    [v , w, r] = deal(v0+sample(tmp1), w0+sample(tmp2), sample(tmp3));
+    [v , w] = deal(v0+sample(tmp1), w0+sample(tmp2));
     x(n) = u1 + v/w * (-sin(theta) + sin(theta + w * delta) );
     y(n) = u2 + v/w * ( cos(theta) - cos(theta + w * delta) );
 end
@@ -34,12 +32,12 @@ hold on;
 
 plot(0, 1, '.');
 hold on;
-plot(0.8*cos(-pi/2:0.1:1-pi/2), 1+0.8*sin(-pi/2:0.1:1-pi/2));
+plot(0.8*cos(-pi/2:0.1:1-pi/2), 1+0.8*sin(-pi/2:0.1:1-pi/2) , 'black');
 hold on;
-plot(1.2*cos(-pi/2:0.1:1-pi/2), 1+1.2*sin(-pi/2:0.1:1-pi/2));
+plot(1.2*cos(-pi/2:0.1:1-pi/2), 1+1.2*sin(-pi/2:0.1:1-pi/2), 'black');
 hold on;
-plot(0.2*cos(0:0.1:2*pi+0.1), 0.2*sin(0:0.1:2*pi+0.1));
+plot(0.2*cos(0:0.1:2*pi+0.1), 0.2*sin(0:0.1:2*pi+0.1), 'black');
 hold on;
-plot(cos(1-pi/2)+0.2*cos(0:0.1:2*pi+0.1), 1+sin(1-pi/2)+0.2*sin(0:0.1:2*pi+0.1));
+plot(cos(1-pi/2)+0.2*cos(0:0.1:2*pi+0.1), 1+sin(1-pi/2)+0.2*sin(0:0.1:2*pi+0.1) , 'black');
 hold on;
 axis equal;
